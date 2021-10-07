94c1e9414fbae82d2296a4fad9c5f68d21bdd3ae
dilipMaharjan <dilipmaharjann@gmail.com>
2021-10-06 21:57:19 -0500

    Post repository and data initialization

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -20,19 +20,23 @@ public class DatabaseLoader implements ApplicationRunner {
             "Earbuds", "Speakers", "Tripod", "Instant Pot", "Coffee Cup", "Keyboard", "Sunglasses"};
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
+    private PostRepository postRepository;
 
-    public DatabaseLoader() {
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository) {
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        IntStream.range(0,40).forEach(i->{
+        IntStream.range(0, 40).forEach(i -> {
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
 
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
+            postRepository.saveAll(randomPosts);
         });
     }
 }
